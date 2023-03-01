c7311be4397c790b385a7cde60e93b22f6a44ce4
Raghuveer N <narahar2@uwm.edu>
2023-02-28 23:32:48 -0800

    add suport for args

--- README.md
+++ README.md
@@ -3,14 +3,28 @@ Granular Tabu Search implementation of DARP for the single/multi CPU in Julia
 
 ## Steps to Run
 1. Install julia
-2. Export env variable (configure it)
+2. Enter into julia shell by running command `julia`
+3. Run below code
+
+```
+use Pkg
+Pkg.add("StatsBase")
+Pkg.add("Random")
+Pkg.add("DateTime")
+Pkg.add("ArgParse")
+```
+
+This is same as pip install, I still couldn't figure out how to generate env files
+for julia env shells and define deps...Might not actually need it since we use
+limited non-std packages
+
+4. Export env variable (configure it)
 
 `export JULIA_NUM_THREADS=4`
 
 This makes Julia runtime to consume only 4 CPU cores
 
-
-3. Run
+5. Run the program
 
 ```
 julia src/main.jl
--- src/main.jl
+++ src/main.jl
@@ -3,15 +3,41 @@ include("construction_kernel.jl")
 include("optimization_fn.jl")
 include("local_search_kernel.jl")
 include("utils.jl")
-using BenchmarkTools
 using Base.Threads
+using ArgParse
 
 # TODO: Tighten bounds for each route
 
 function main()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--nR"
+            help = "number of requests"
+            arg_type = Int64
+            default = 50
+        "--sd"
+            help = "service duration"
+            arg_type = Int64
+            default = 2
+        "--aos"
+            help = "area of service in Kms"
+            arg_type = Int64
+            default = 10
+        "--nV"
+            help = "number of vehicles"
+            arg_type = Int64
+            default = 10
+        "--Q"
+            help = "vehicle capacity"
+            arg_type = Int64
+            default = 5
+    end
+    args = parse_args(s)
+    println(args)
     start_dt = now()
     println("Running on $(Threads.nthreads()) threads")
-    darp = DARP(500, 2, 10, 5, 1)
+    # darp = DARP(500, 2, 10, 5, 1)
+    darp = DARP(args["nR"], args["sd"], args["aos"], args["nV"], args["Q"])
     N_SIZE = 0.75 * darp.nR
     N_SIZE = trunc(Int64, N_SIZE)
     total_iterations = darp.nR * 10
@@ -29,7 +55,7 @@ function main()
     local_search(darp, total_iterations, N_SIZE, routes[idx])
     local_search_completed_dt = now()
     println("Local Search ($(total_iterations) iterations) took => $(ts_diff(init_route_complete_dt, local_search_completed_dt))")
-    println("Total Time => $(ts_diff(local_search_completed_dt, start_dt))")
+    println("Total Time => $(ts_diff(start_dt, local_search_completed_dt))")
 end
 
 main()
