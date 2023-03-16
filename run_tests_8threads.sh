d34018d2c87c8f2edb228f0e916826bd00a374d0
Raghuveer N <narahar2@uwm.edu>
2023-03-15 22:07:44 -0700

    fix runtests

--- run_tests_8threads.sh
+++ run_tests_8threads.sh
@@ -1,12 +1,3 @@
-export JULIA_NUM_THREADS=30
-julia src/test_runner.jl --statsfile $1.csv
-
-export JULIA_NUM_THREADS=24
-julia src/test_runner.jl --statsfile $1.csv
-
-export JULIA_NUM_THREADS=16
-julia src/test_runner.jl --statsfile $1.csv
-
 export JULIA_NUM_THREADS=8
 julia src/test_runner.jl --statsfile $1.csv
 
