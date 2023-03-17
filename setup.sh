36b3271a3007bd0f62834a00840971428346cc0d
Raghuveer N <narahar2@uwm.edu>
2023-03-16 21:28:04 -0700

    set

---
+++ setup.sh
@@ -0,0 +1,11 @@
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.1-linux-x86_64.tar.gz
+tar zxvf julia-1.8.1-linux-x86_64.tar.gz
+export PATH='$PATH:~/julia-1.8.1/bin/'
+git clone https://github.com/nraghuveer/DARP-Multi-CPU.git
+cd DARP-Multi-CPU
+chmod +x run_tests_adv.sh
+
+julia -e 'using Pkg; Pkg.add("StatsBase"); Pkg.add("Random"); Pkg.add("CSV"); Pkg.add("ArgParse")'
+
+echo "Done......"
+
