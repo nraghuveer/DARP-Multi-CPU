eaa3807abc0b60d984d76f6b836a34f51738ebc9
Raghuveer Naraharisetti <narahar2@uwm.edu>
2023-03-25 11:40:43 -0700

    staticarrays

--- stats_analysis.py
+++ stats_analysis.py
@@ -10,8 +10,9 @@ requests = [50, 350, 500, 650, 100]
 
 def main():
     filename = sys.argv[1]
-    sd = int(sys.argv[2])
+    nR = int(sys.argv[2])
     aos = 10
+    sd = 2
     # fig, axs = plt.subplots(len(service_duration))
     # fig.suptitle(f'Heading here')
     # for i, sd in enumerate(service_duration):
@@ -21,7 +22,8 @@ def main():
     # # plt.legend([f"{t} Threads" for t in threads], loc='upper left')
     # plt.show()
 
-    compare_tt_across_threads(filename, threads, aos, sd)
+    # compare_tt_across_threads(filename, threads, aos, sd)
+    show_speedup(filename, [1,2,4,8,16,32,56], nR, aos, sd)
 
 def add_plot(ax, filename, threads, aos, sd):
     df = pd.read_csv(filename)
@@ -35,8 +37,39 @@ def add_plot(ax, filename, threads, aos, sd):
 
 
 
+def show_speedup(filename, threads, nR, aos, sd):
+    threads.sort()
+    def getTime(t, nR, df):
+        x = [row['time_total'] for _, row in df.iterrows() if (int(row['nR']) == nR and int(row['nThreads']==t))]
+        return x[0] if x else None
+    df = pd.read_csv(filename, header=0)
+    plt.suptitle("Speedups")
+    plt.xlabel("Threads")
+    plt.ylabel("Speedup")
+    base = getTime(1, nR, df)
+
+    values = []
+    speedup = {}
+    for thread in threads:
+        if thread == 1:
+            continue
+        # for each thread, get base value and calc speedup
+        cur = getTime(thread, nR, df)
+        if not cur:
+            continue
+        print(f"thread={thread} | speedup={cur/base} | cur={cur}| base={base}")
+        values.append((thread, base/cur))
+
+    values.sort(key=lambda x: x[0])
+    print(values)
+    xvalues = [x[0] for x in values]
+    yvalues = [x[1] for x in values]
+    plt.plot(xvalues, yvalues)
+    plt.legend([f"{t} Threads" for t in threads], loc='upper left')
+    plt.savefig("plot.png")
+
 def compare_tt_across_threads(filename, threads, aos, sd):
-    df = pd.read_csv(filename)
+    df = pd.read_csv(filename, header=0)
     plt.suptitle("Varying total times with differnt thread configurations")
     plt.xlabel("Number of requests")
     plt.ylabel("Total Time")
